d38e0c0272a16f0499c8c12b8ddda0e777e9e6a8
Aashesh Kumar <Aashesh.sharma389@gmail.com>
2025-01-19 15:15:46 +0500

    Add files via upload

---
+++ responsivenav.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
+const buttons = document.querySelectorAll('.nav-links button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
